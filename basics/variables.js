1913ee22fd0dbcd92e45796543c6cf6f0234519c
avinashbhujbal09 <83542005+avinashbhujbal09@users.noreply.github.com>
2024-07-24 12:08:28 +0000

    variableLs

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = "123"
+let accountEmail ="avinash@gmail.com"
+var accountPass = "123456"
+accountCity = "Mumbai"
+
+
+
+console.table([accountEmail, accountId, accountPass, accountCity])
